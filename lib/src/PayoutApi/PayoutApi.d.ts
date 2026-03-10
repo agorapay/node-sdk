1bc3de8c0b806b391829d4709a4ef54c92b746a4
caps-lro <164214280+caps-lro@users.noreply.github.com>
2026-03-10 12:09:58 +0100

    Version 12.1.1 : /lib/src part. 2

---
+++ lib/src/PayoutApi/PayoutApi.d.ts
@@ -0,0 +1,32 @@
+import ApiRest from '../../utils/apiRest';
+import { CreatePayoutOptions } from './PayoutApiInterfaces';
+declare class PayoutApi extends ApiRest {
+    /**
+     * Ask for a payout.
+     * @description Submit a payout for a specific account.
+     * @param {CreatePayoutOptions} options payout creation options.
+     * @prop {string | undefined} endToEndId - Use to identify transaction in SEPA transfer.
+     * @prop {Amount} payoutAmount - The payout amount.
+     * @prop {string} paymentMethodAlias - Alias for the payment method.
+     * @prop {string} accountNumber - A string representing the account number.
+     * @prop {Commission | undefined} commission - The payout commission.
+     * @prop {string | undefined} metaData - JSON data for the marketplace. This data is not used by payment system.
+     * @prop {string | undefined} reason - Operation label transmitted in payment system. Maximum length of 140 characters.
+     * @returns {number} The payout transaction Id.
+     * @example
+     * ````javascript
+      payoutApi.createPayout({
+        endToEndId: "1",
+        accountNumber: "12345678",
+        paymentMethodAlias: "12334566",
+        payoutAmount: new Amount(10000, "EUR")
+      }).then(resp => {
+        console.log(resp)
+      }).catch(error => {
+        console.log(error)
+      })
+     * ````
+     */
+    createPayout(options: CreatePayoutOptions): Promise<number>;
+}
+export default PayoutApi;
---
+++ lib/src/TransferApi/TransferApiInterfaces.d.ts
@@ -0,0 +1,24 @@
+import Amount from '../models/Amount';
+/**
+ * @prop {string} accountCptNumber - A string representing the account number.
+ * @prop {Amount} transferAmount - The transfer amount.
+ * @prop {string} accountNumber - A string representing the account number.
+ * @prop {string | undefined} orderRef - Marketplace reference for this order. Characters authorized are: a to z, A to Z, 0 to 9 and - / . + _ and space.
+ * @prop {string | undefined} metaData - JSON data for the marketplace. This data is not used by payment system.
+ * @prop {string} reason - Operation label transmitted in payment system. Maximum length of 140 characters.
+ */
+interface CreateTransferOptions {
+    /** A string representing the account number. */
+    accountCptNumber: string;
+    /** The transfer amount. */
+    transferAmount: Amount;
+    /** A string representing the account number. */
+    accountNumber: string;
+    /** Marketplace reference for this order. Characters authorized are: a to z, A to Z, 0 to 9 and - / . + _ and space. */
+    orderRef?: string;
+    /** JSON data for the marketplace. This data is not used by payment system. */
+    metaData?: string;
+    /** Operation label transmitted in payment system. Maximum length of 140 characters. */
+    reason: string;
+}
+export { CreateTransferOptions };
